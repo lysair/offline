1fb9e2e82223a7d09145b6e51bea4fdc10f303d5
Mechanel <nyutanislavsky@qq.com>
2023-08-16 00:40:14 +0800

    add 风花雪月刘宏

--- ofl.lua
+++ ofl.lua
@@ -3,6 +3,7 @@ extension.extensionName = "offline"
 
 Fk:loadTranslationTable{
   ["ofl"] = "线下",
+  ["rom"] = "风花雪月",
 }
 
 local caesar = General(extension, "caesar", "god", 4)
@@ -51,6 +52,51 @@ Fk:loadTranslationTable{
   ["#conqueror-choice"] = "Conqueror: You may declare a card type, %dest shall give you a card of the declared type<br>"..
   "to negate the effect, or the %arg may not be Dodged",
   ["#conqueror-give"] = "Conqueror: You shall give %src a %arg to negate the effect, or the Strike may not be Dodged",
+}
+
+local liuhong = General(extension, "rom__liuhong", "qun", 4)
+
+local rom__zhenglian = fk.CreateTriggerSkill{
+  name = "rom__zhenglian",
+  events = {fk.EventPhaseStart},
+  anim_type = "control",
+  can_trigger = function(self, event, target, player, data)
+    return target == player and player:hasSkill(self.name) and player.phase == Player.Start 
+  end,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    local targets = room:getOtherPlayers(player)
+    local prompt = "#rom__zhenglian-ask:" .. player.id
+    local tos = {}
+    for _, p in ipairs(targets) do
+      if player.dead then return end
+      if not p.dead then
+        local card = room:askForCard(p, 1, 1, true, self.name, true, nil, prompt)
+        if #card > 0 then
+          room:moveCardTo(card, Card.PlayerHand, player, fk.ReasonGive, self.name, nil, false, player.id)
+        else
+          table.insert(tos, p)
+        end
+      end
+    end
+    local num = #tos
+    tos = table.map(table.filter(tos, function(p) return not p:isNude() end), Util.IdMapper)
+    local to = room:askForChoosePlayers(player, tos, 1, 1, "#rom__zhenglian-discard:::" .. num, self.name, true)
+    if #to > 0 then
+      room:askForDiscard(room:getPlayerById(to[1]), num, num, true, self.name, false)
+    end
+  end
+}
+
+liuhong:addSkill(rom__zhenglian)
+
+Fk:loadTranslationTable{
+  ["rom__liuhong"] = "刘宏",
+  ["rom__zhenglian"] = "征敛",
+  [":rom__zhenglian"] = "准备阶段，你可以令所有其他角色依次选择是否交给你一张牌。所有角色选择完毕后，你可以令一名选择否的角色弃置X张牌（X为选择否的角色数）。",
+
+  ["#rom__zhenglian-ask"] = "征敛：交给 %src 一张牌，否则有可能被其要求弃牌",
+  ["#rom__zhenglian-discard"] = "征敛：你可以令一名选择否的角色弃置 %arg 张牌",
 }
 
 return extension
