0ad819bbc82ea79468717fc9c247f1f40097502c
RalphR <1715333874@qq.com>
2023-08-22 17:11:27 +0800

    孙寒华

--- ofl.lua
+++ ofl.lua
@@ -54,6 +54,212 @@ Fk:loadTranslationTable{
   ["#conqueror-give"] = "Conqueror: You shall give %src a %arg to negate the effect, or the Strike may not be Dodged",
 }
 
+local sunhanhua = General(extension, "ofl__sunhanhua", "wu", 3, 3, General.Female)
+local ofl__chongxu = fk.CreateActiveSkill{
+  name = "ofl__chongxu",
+  anim_type = "drawcard",
+  card_num = 0,
+  target_num = 0,
+  prompt = "#ofl__chongxu",
+  interaction = function()
+    return UI.ComboBox {choices = {"ofl__chongxu_yes", "ofl__chongxu_no"}}
+  end,
+  can_use = function(self, player)
+    return player:usedSkillTimes(self.name, Player.HistoryPhase) == 0
+  end,
+  card_filter = function(self, to_select, selected)
+    return false
+  end,
+  target_filter = function(self, to_select, selected, selected_cards)
+    return false
+  end,
+  on_use = function(self, room, effect)
+    local player = room:getPlayerById(effect.from)
+    local cards = room:getNCards(2)
+    room:moveCards({
+      ids = cards,
+      toArea = Card.Processing,
+      moveReason = fk.ReasonJustMove,
+      skillName = self.name,
+    })
+    room:sendCardVirtName(cards, self.name)
+    room:delay(2000)
+    if (self.interaction.data == "ofl__chongxu_yes" and Fk:getCardById(cards[1]).color == Fk:getCardById(cards[2]).color) or
+      (self.interaction.data == "ofl__chongxu_no" and Fk:getCardById(cards[1]).color ~= Fk:getCardById(cards[2]).color) then
+      local all_choices = {"ofl__chongxu_get", "miaojian_update", "lianhuas_update"}
+      local choices = table.simpleClone(all_choices)
+      if player:getMark("@miaojian") == "miaojian3" then
+        table.removeOne(choices, "miaojian_update")
+      end
+      if player:getMark("@lianhuas") == "lianhuas3" then
+        table.removeOne(choices, "lianhuas_update")
+      end
+      local choice = room:askForChoice(player, choices, self.name, "", false, all_choices)
+      if choice == "ofl__chongxu_get" then
+        local dummy = Fk:cloneCard("dilu")
+        dummy:addSubcards(cards)
+        room:obtainCard(player, dummy, true, fk.ReasonJustMove)
+      else
+        if choice == "miaojian_update" then
+          if player:getMark("@miaojian") == 0 then
+            room:setPlayerMark(player, "@miaojian", "miaojian2")
+          else
+            room:setPlayerMark(player, "@miaojian", "miaojian3")
+          end
+        elseif choice == "lianhuas_update" then
+          if player:getMark("@lianhuas") == 0 then
+            room:setPlayerMark(player, "@lianhuas", "lianhuas2")
+          else
+            room:setPlayerMark(player, "@lianhuas", "lianhuas3")
+          end
+        end
+        room:moveCards({
+          ids = cards,
+          toArea = Card.DiscardPile,
+          moveReason = fk.ReasonJustMove,
+          skillName = self.name,
+        })
+      end
+    else
+      room:fillAG(player, cards)
+      local id = room:askForAG(player, cards, false, self.name)
+      room:closeAG(player)
+      if not id then return false end
+      table.removeOne(cards, id)
+      room:obtainCard(player, id, true, fk.ReasonJustMove)
+      room:moveCards({
+        ids = cards,
+        toArea = Card.DiscardPile,
+        moveReason = fk.ReasonJustMove,
+        skillName = self.name,
+      })
+    end
+  end,
+}
+local miaojian = fk.CreateViewAsSkill{
+  name = "miaojian",
+  prompt = function (self, selected, selected_cards)
+    if Self:getMark("@miaojian") == 0 then
+      return "#miaojian1"
+    elseif Self:getMark("@miaojian") == "miaojian2" then
+      return "#miaojian2"
+    elseif Self:getMark("@miaojian") == "miaojian3" then
+      return "#miaojian3"
+    end
+  end,
+  interaction = function()
+    return UI.ComboBox {choices = {"stab__slash", "ex_nihilo"}}
+  end,
+  card_filter = function(self, to_select, selected)
+    if #selected == 0 then
+      local card = Fk:getCardById(to_select)
+      if Self:getMark("@miaojian") == 0 then
+        if self.interaction.data == "stab__slash" then
+          return card.trueName == "slash"
+        elseif self.interaction.data == "ex_nihilo" then
+          return card.type == Card.TypeTrick
+        end
+      elseif Self:getMark("@miaojian") == "miaojian2" then
+        if self.interaction.data == "stab__slash" then
+          return card.type == Card.TypeBasic
+        elseif self.interaction.data == "ex_nihilo" then
+          return card.type ~= Card.TypeBasic
+        end
+      elseif Self:getMark("@miaojian") == "miaojian3" then
+        return false
+      end
+    end
+  end,
+  view_as = function(self, cards)
+    if not self.interaction.data then return end
+    local card = Fk:cloneCard(self.interaction.data)
+    if Self:getMark("@miaojian") == 0 or Self:getMark("@miaojian") == "miaojian2" then
+      if #cards ~= 1 then return end
+      card:addSubcard(cards[1])
+    end
+    card.skillName = self.name
+    return card
+  end,
+  enabled_at_play = function(self, player)
+    return player:usedSkillTimes(self.name, Player.HistoryPhase) == 0
+  end,
+  enabled_at_response = function(self, player)
+    return false
+  end,
+}
+local lianhuas = fk.CreateTriggerSkill{
+  name = "lianhuas",
+  events = {fk.TargetConfirming},
+  anim_type = "defensive",
+  can_trigger = function(self, event, target, player, data)
+    return target == player and player:hasSkill(self.name) and data.card.trueName == "slash"
+  end,
+  on_cost = function(self, event, target, player, data)
+    return true
+  end,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    player:drawCards(1, self.name)
+    if player.dead or player:getMark("@lianhuas") == 0 then return end
+    if player:getMark("@lianhuas") == "lianhuas2" then
+      local judge = {
+        who = player,
+        reason = self.name,
+        pattern = ".|.|spade",
+      }
+      room:judge(judge)
+      if judge.card.suit == Card.Spade then
+        AimGroup:cancelTarget(data, player.id)
+      end
+    elseif player:getMark("@lianhuas") == "lianhuas3" then
+      local from = room:getPlayerById(data.from)
+      if from.dead or from:isNude() or
+        #room:askForDiscard(from, 1, 1, true, self.name, true, ".", "#lianhuas-discard:"..player.id) == 0 then
+        AimGroup:cancelTarget(data, player.id)
+      end
+    end
+  end,
+}
+sunhanhua:addSkill(ofl__chongxu)
+sunhanhua:addSkill(miaojian)
+sunhanhua:addSkill(lianhuas)
+Fk:loadTranslationTable{
+  ["ofl__sunhanhua"] = "孙寒华",
+  ["ofl__chongxu"] = "冲虚",
+  [":ofl__chongxu"] = "出牌阶段限一次，你可以猜测牌堆顶两张牌颜色是否相同，然后亮出之，若你猜对，你可以选择一项：1.获得之；2.修改〖妙剑〗；"..
+  "3.修改〖莲华〗；若你猜错，你可以获得其中一张牌。",
+  ["miaojian"] = "妙剑",
+  [":miaojian"] = "出牌阶段限一次，你可以将【杀】当刺【杀】、锦囊牌当【无中生有】使用。<br>二阶：出牌阶段限一次，你可以将基本牌当刺【杀】、"..
+  "非基本牌当【无中生有】使用。<br>三阶：出牌阶段限一次，你可以视为使用一张刺【杀】或【无中生有】。",
+  ["lianhuas"] = "莲华",
+  [":lianhuas"] = "当你成为【杀】的目标时，你摸一张牌。<br>二阶：当你成为【杀】的目标时，你摸一张牌，然后你判定，若为♠，取消之。<br>"..
+  "三阶：当你成为【杀】的目标时，你摸一张牌，然后使用者需弃置一张牌，否则取消之。",
+  ["#ofl__chongxu"] = "冲虚：猜测牌堆顶两张牌颜色是否相同，猜对获得之或升级技能，猜错获得其中一张",
+  ["ofl__chongxu_yes"] = "相同",
+  ["ofl__chongxu_no"] = "不同",
+  ["ofl__chongxu_get"] = "获得这些牌",
+  ["miaojian_update"] = "升级〖妙剑〗",
+  ["lianhuas_update"] = "升级〖莲华〗",
+  ["#miaojian1"] = "妙剑：你可以将【杀】当刺【杀】、锦囊牌当【无中生有】使用",
+  ["#miaojian2"] = "妙剑：你可以将基本牌当刺【杀】、非基本牌当【无中生有】使用",
+  ["#miaojian3"] = "妙剑：你可以视为使用一张刺【杀】或【无中生有】",
+  ["@miaojian"] = "妙剑",
+  ["miaojian2"] = "妙剑二阶",
+  ["miaojian3"] = "妙剑三阶",
+  ["@lianhuas"] = "莲华",
+  ["lianhuas2"] = "莲华二阶",
+  ["lianhuas3"] = "莲华三阶",
+  ["#lianhuas-discard"] = "莲华：你需弃置一张牌，否则 %src 取消此【杀】",
+
+  ["$ofl__chongxu1"] = "阳炁冲三关，斩尸除阴魔。",
+  ["$ofl__chongxu2"] = "蒲团清静坐，神归了道真。",
+  ["$miaojian1"] = "谨以三尺玄锋，代天行化，布令宣威。",
+  ["$miaojian2"] = "布天罡，踏北斗，有秽皆除，无妖不斩。",
+  ["$lianhuas1"] = "刀兵水火，速离身形。",
+  ["$lianhuas2"] = "体有金光，覆映全身。",
+  ["~ofl__sunhanhua"] = "天有寒暑，人有死生……",
+}
+
 local liuhong = General(extension, "rom__liuhong", "qun", 4)
 
 local rom__zhenglian = fk.CreateTriggerSkill{
