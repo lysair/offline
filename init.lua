e470decb2172e6493a82a63290b7ef82c84ae100
notify <notify-ctrl@qq.com>
2024-01-26 15:43:55 +0800

    大包翻译

--- init.lua
+++ init.lua
@@ -2,6 +2,8 @@ local ofl_other = require "packages/offline/ofl_other"
 local espionage_beta = require "packages/offline/espionage_beta"
 local fhyx = require "packages/offline/fhyx"
 
+Fk:loadTranslationTable{ ["offline"] = "线下" }
+
 return {
   ofl_other,
   espionage_beta,
