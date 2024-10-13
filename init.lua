5f7f2dcf7588bdbf6fb9576ad9313a2d02a59618
RalphR <1715333874@qq.com>
2024-10-13 21:39:40 +0800

    四象封印：少阳

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ local jiuding = require "packages/offline/jiuding"
 local shzj = require "packages/offline/shzj"
 local sxfy_shaoyin = require "packages/offline/sxfy_shaoyin"
 local sxfy_taiyin = require "packages/offline/sxfy_taiyin"
+local sxfy_shaoyang = require "packages/offline/sxfy_shaoyang"
 local tqt = require "packages/offline/assassins"
 local zyz = require "packages/offline/bgmdiy"
 
@@ -20,6 +21,7 @@ return {
   shzj,
   sxfy_shaoyin,
   sxfy_taiyin,
+  sxfy_shaoyang,
   tqt,
   zyz,
 
