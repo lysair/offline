57b8881af038864fd16afb5151c42a753b3b2cd4
RalphR <1715333874@qq.com>
2024-12-09 15:24:24 +0800

    牢角

--- ofl_other2.lua
+++ ofl_other2.lua
@@ -9,7 +9,6 @@ Fk:loadTranslationTable{
 
 --官盗E5：风云志·汉末风云
 local godzhangjiao = General(extension, "ofl__godzhangjiao", "god", 4)
-godzhangjiao.total_hidden = true
 local sanshou = fk.CreateTriggerSkill{
   name = "ofl__sanshou",
   anim_type = "special",
@@ -51,6 +50,208 @@ local sanshou = fk.CreateTriggerSkill{
     player.room:changeHero(player, "ofl__godzhangjiao", false, player:getMark("ofl__sanshou-phase") == 2, true, false, false)
   end,
 }
+local mingdao_mapper = {"weapon", "armor", "defensive_horse", "offensive_horse"}
+local mingdao = fk.CreateTriggerSkill{
+  name = "ofl__mingdao",
+  anim_type = "special",
+  events = {fk.GameStart},
+  can_trigger = function(self, event, target, player, data)
+    return player:hasSkill(self) and
+      table.find({3, 4, 5, 6}, function (sub_type)
+        return player:hasEmptyEquipSlot(sub_type)
+      end)
+  end,
+  on_cost = function (self, event, target, player, data)
+    local room = player.room
+    if not room:getBanner(self.name) then
+      room:setBanner(self.name, {
+        room:printCard("weapon1__populace", Card.Heart, 1).id,
+        room:printCard("armor1__populace", Card.Diamond, 1).id,
+        room:printCard("defensive_horse1__populace", Card.Club, 1).id,
+        room:printCard("offensive_horse1__populace", Card.Spade, 1).id,
+      })
+    end
+    local success, dat = room:askForUseActiveSkill(player, "ofl__mingdao_active", "#ofl__mingdao-invoke", true)
+    if success and dat then
+      self.cost_data = dat
+      return true
+    end
+  end,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    local index = table.indexOf(mingdao_mapper, string.sub(self.cost_data.interaction, 6))
+    local skill = string.split(Fk:getCardById(self.cost_data.cards[1]).name, "1")[1]
+    local name = skill..tostring(index).."__populace"
+    local suit
+    if skill == "weapon" then
+      suit = Card.Heart
+    elseif skill == "armor" then
+      suit = Card.Diamond
+    elseif skill == "defensive_horse" then
+      suit = Card.Club
+    elseif skill == "offensive_horse" then
+      suit = Card.Spade
+    end
+    local tag = room.tag[self.name] or {}
+    local card = table.find(tag, function (id)
+      local c = Fk:getCardById(id)
+      return room:getCardArea(id) == Card.Void and c.name == name and c.suit == suit
+    end)
+    if card == nil then
+      local id = room:printCard(name, suit, 1).id
+      table.insert(tag, id)
+      room:setTag(self.name, tag)
+      card = id
+    end
+    room:setCardMark(Fk:getCardById(card), MarkEnum.DestructIntoDiscard, 1)
+    room:moveCardIntoEquip(player, card, self.name, false, player.id)
+  end,
+}
+local mingdao_active = fk.CreateActiveSkill{
+  name = "ofl__mingdao_active",
+  card_num = 1,
+  target_num = 0,
+  expand_pile = function (self)
+    return Fk:currentRoom():getBanner("ofl__mingdao")
+  end,
+  interaction = function()
+    local choices = {}
+    for _, sub_type in ipairs({3, 4, 5, 6}) do
+      if Self:hasEmptyEquipSlot(sub_type) then
+        for i = 1, #Self:getAvailableEquipSlots(sub_type) - #Self:getEquipments(sub_type), 1 do
+          table.insert(choices, "type_"..mingdao_mapper[sub_type - 2])
+        end
+      end
+    end
+    return UI.ComboBox { choices = choices }
+  end,
+  card_filter = function(self, to_select, selected)
+    return #selected == 0 and table.contains(Fk:currentRoom():getBanner("ofl__mingdao"), to_select)
+  end,
+}
+local zhongfu = fk.CreateTriggerSkill{
+  name = "ofl__zhongfu",
+  anim_type = "support",
+  events = {fk.RoundStart},
+  can_trigger = function(self, event, target, player, data)
+    return player:hasSkill(self)
+  end,
+  on_cost = function (self, event, target, player, data)
+    local room = player.room
+    local choice = room:askForChoice(player, {"log_spade", "log_heart", "log_club", "log_diamond", "Cancel"}, self.name,
+      "#ofl__zhongfu-choice")
+    if choice ~= "Cancel" then
+      local targets = table.filter(room.alive_players, function (p)
+        return table.every(room.alive_players, function (q)
+          return p:getHandcardNum() <= q:getHandcardNum()
+        end)
+      end)
+      self.cost_data = {tos = table.map(targets, Util.IdMapper), choice = choice}
+      return true
+    end
+  end,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    room:setPlayerMark(player, "@ofl__zhongfu-round", self.cost_data.choice)
+    for _, id in ipairs(self.cost_data.tos) do
+      local p = room:getPlayerById(id)
+      if not p.dead then
+        if p:isNude() then
+          p:drawCards(1, self.name, "bottom")
+        else
+          local card = room:askForCard(p, 1, 1, true, self.name, true, nil, "#ofl__zhongfu-ask:"..player.id)
+          if #card > 0 then
+            if not player.dead then
+              room:addTableMark(player, "ofl__zhongfu-round", id)
+              room:setPlayerMark(p, "@@ofl__zhongfu_target-round", 1)
+            end
+            room:moveCards({
+              ids = card,
+              from = id,
+              toArea = Card.DrawPile,
+              moveReason = fk.ReasonPut,
+              skillName = self.name,
+              drawPilePosition = 1,
+            })
+          else
+            p:drawCards(1, self.name, "bottom")
+          end
+        end
+      end
+    end
+  end,
+}
+local zhongfu_delay = fk.CreateTriggerSkill{
+  name = "#ofl__zhongfu_delay",
+  anim_type = "support",
+  events = {fk.Damage},
+  can_trigger = function(self, event, target, player, data)
+    return target and table.contains(player:getTableMark("ofl__zhongfu-round"), target.id) and
+      table.find({3, 4, 5, 6}, function (sub_type)
+        return player:hasEmptyEquipSlot(sub_type)
+      end)
+  end,
+  on_cost = Util.TrueFunc,
+  on_use = function (self, event, target, player, data)
+    mingdao:doCost(event, target, player, data)
+  end,
+}
+local dangjing = fk.CreateTriggerSkill{
+  name = "ofl__dangjing",
+  anim_type = "offensive",
+  events = {fk.AfterSkillEffect},
+  can_trigger = function(self, event, target, player, data)
+    return target == player and player:hasSkill(self) and (data.name == "ofl__zhongfu" or data.name == "#ofl__zhongfu_delay") and
+      table.every(player.room.alive_players, function (p)
+        return #player:getCardIds("e") >= #p:getCardIds("e")
+      end)
+  end,
+  on_cost = function(self, event, target, player, data)
+    local room = player.room
+    local to = room:askForChoosePlayers(player, table.map(room.alive_players, Util.IdMapper), 1, 1,
+      "#ofl__dangjing-choose", self.name, true)
+    if #to > 0 then
+      self.cost_data = {tos = to}
+      return true
+    end
+  end,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    local to = room:getPlayerById(self.cost_data.tos[1])
+    local mark = player:getMark("@ofl__zhongfu-round")
+    local pattern
+    if mark == 0 then
+      pattern = "false"
+    else
+      pattern = ".|.|"..U.ConvertSuit(mark, "sym", "str")
+    end
+    local judge = {
+      who = to,
+      reason = self.name,
+      pattern = pattern,
+    }
+    room:judge(judge)
+    if judge.card.suit == U.ConvertSuit(mark, "sym", "int") then
+      if not to.dead then
+        room:damage{
+          from = player,
+          to = to,
+          damage = 1,
+          damageType = fk.ThunderDamage,
+          skillName = self.name,
+        }
+      end
+      if not player.dead then
+        self:doCost(event, target, player, data)
+      end
+    end
+  end,
+}
+Fk:addSkill(mingdao_active)
+zhongfu:addRelatedSkill(zhongfu_delay)
+godzhangjiao:addSkill(mingdao)
+godzhangjiao:addSkill(zhongfu)
+godzhangjiao:addSkill(dangjing)
 godzhangjiao:addSkill(sanshou)
 Fk:loadTranslationTable{
   ["ofl__godzhangjiao"] = "神张角",
@@ -58,16 +259,23 @@ Fk:loadTranslationTable{
   ["illustrator:ofl__godzhangjiao"] = "鬼画府",
 
   ["ofl__mingdao"] = "瞑道",
-  [":ofl__mingdao"] = "游戏开始时，你可以将一张“众”置入你的装备区。",
+  [":ofl__mingdao"] = "游戏开始时，你可以将一张“众”置入你的装备区，“众”进入弃牌堆时销毁。",
   ["ofl__zhongfu"] = "众附",
-  [":ofl__zhongfu"] = "每轮开始时，你可以声明一种花色，然后令手牌最少的角色依次选择一项：1.将一张牌置于牌堆顶；2.从牌堆底摸一张牌，"..
-  "当以此法失去牌的角色造成伤害，你发动一次〖瞑道〗。",
+  [":ofl__zhongfu"] = "每轮开始时，你可以声明一种花色，然后令手牌最少的角色依次选择一项：1.将一张牌置于牌堆顶；2.从牌堆底摸一张牌。"..
+  "本轮当以此法失去牌的角色造成伤害后，你可以发动一次〖瞑道〗。",
   ["ofl__dangjing"] = "荡京",
   [":ofl__dangjing"] = "当你发动〖众附〗后，若你装备区内的牌为全场最多，你可以令一名角色进行一次判定，若为你〖众附〗声明的花色，你对其造成1点"..
-  "雷电伤害并重复此流程。",
+  "雷电伤害且可以重复此流程。",
   ["ofl__sanshou"] = "三首",
   [":ofl__sanshou"] = "锁定技，你的准备阶段和结束阶段改为出牌阶段，并在此阶段将武将牌改为张宝或张梁。此阶段结束后把武将牌替换回张角。",
-
+  ["ofl__mingdao_active"] = "瞑道",
+  ["#ofl__mingdao-invoke"] = "瞑道：将一张“众”置入你的装备区（选择一种“众”及副类别，右键/长按可查看技能）",
+  ["#ofl__zhongfu-choice"] = "众附：你可以声明本轮生效的“众附”花色，然后令手牌数最少的角色依次选择一项",
+  ["@ofl__zhongfu-round"] = "众附",
+  ["#ofl__zhongfu-ask"] = "众附：点“取消”摸一张牌；或将一张牌置于牌堆顶，本轮你造成伤害后 %src 可发动“瞑道”",
+  ["#ofl__zhongfu_delay"] = "众附",
+  ["@@ofl__zhongfu_target-round"] = "信众",
+  ["#ofl__dangjing-choose"] = "荡京：令一名角色进行判定，若为“众附”花色，对其造成1点雷电伤害且可以再次发动！",
   ["#ofl__sanshou-choose"] = "三首：选择此阶段要变为的武将",
 }
 
@@ -131,17 +339,21 @@ local zhaobing = fk.CreateProhibitSkill{
   prohibit_use = function(self, player, card)
     if not player:hasSkill(self) then
       local subcards = card:isVirtual() and card.subcards or {card.id}
-      return #subcards > 0 and (table.find(subcards, function(id)
+      if #subcards > 0 and table.find(subcards, function(id)
         return table.contains(player:getPile("ofl__zhoubing&"), id)
-      end) ~= nil or player.phase ~= Player.Play)
+      end) then
+        return player.phase ~= Player.Play
+      end
     end
   end,
   prohibit_response = function(self, player, card)
     if not player:hasSkill(self) then
       local subcards = card:isVirtual() and card.subcards or {card.id}
-      return #subcards > 0 and (table.find(subcards, function(id)
+      if #subcards > 0 and table.find(subcards, function(id)
         return table.contains(player:getPile("ofl__zhoubing&"), id)
-      end) ~= nil or player.phase ~= Player.Play)
+      end) then
+        return player.phase ~= Player.Play
+      end
     end
   end,
 }
--- ofl_token.lua
+++ ofl_token.lua
@@ -290,21 +290,18 @@ local ironBudSkill = fk.CreateTriggerSkill{
     player.room:setPlayerMark(player, "iron_bud-turn", player.hp)
   end,
 }
-local iron_bud_attackrange = fk.CreateAttackRangeSkill{
-  name = "#iron_bud_attackrange",
-  correct_func = function (self, from, to)
-    if from:hasSkill(ironBudSkill) and from:usedSkillTimes("#iron_bud_skill", Player.HistoryTurn) > 0 then
-      return from:getMark("iron_bud-turn") - 2
-    end
-  end,
-}
-ironBudSkill:addRelatedSkill(iron_bud_attackrange)
 Fk:addSkill(ironBudSkill)
 local ironBud = fk.CreateWeapon{
   name = "&iron_bud",
   suit = Card.Spade,
   number = 5,
-  attack_range = 2,
+  dynamic_attack_range = function(self, player)
+    if player then
+      local mark = player:getMark("iron_bud-turn")
+      return mark > 0 and mark or 2
+    end
+    return 2
+  end,
   equip_skill = ironBudSkill,
   on_uninstall = function(self, room, player)
     Weapon.onUninstall(self, room, player)
@@ -393,5 +390,157 @@ Fk:loadTranslationTable{
   ["#shzj__burning_camps_skill"] = "选择一名有牌的角色，展示其一张牌，<br/>然后你可以弃置一张花色相同的手牌对其造成1点火焰伤害并弃置其展示牌",
   ["#shzj__burning_camps-show"] = "火烧连营：展示 %dest 一张牌",
 }
+
+local PopulaceSkill = fk.CreateTriggerSkill{
+  name = "#populace_skill",
+  attached_equip = "weapon1__populace",
+  frequency = Skill.Compulsory,
+  events = {fk.Damaged},
+  can_trigger = function(self, event, target, player, data)
+    if target == player then
+      for _, id in ipairs(player:getCardIds("e")) do
+        if Fk:getCardById(id).trueName == "populace" then
+          return true
+        end
+      end
+    end
+  end,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    for _, id in ipairs(player:getCardIds("e")) do
+      if player.dead then return end
+      local equip = Fk:getCardById(id)
+      if equip.trueName == "populace" then
+        if equip.name:startsWith("weapon") then
+          room:addPlayerMark(player, MarkEnum.SlashResidue, 1)
+          room:addPlayerMark(player, "@populace_attackrange", 1)
+        elseif equip.name:startsWith("armor") then
+          player:drawCards(2, self.name)
+        elseif equip.name:startsWith("defensive_horse") then
+          room:addPlayerMark(player, MarkEnum.AddMaxCards, 1)
+        elseif equip.name:startsWith("offensive_horse") then
+          room:addPlayerMark(player, "@populace_distance", 1)
+        end
+      end
+    end
+  end,
+}
+local populace_attackrange = fk.CreateAttackRangeSkill{
+  name = "#populace_attackrange",
+  frequency = Skill.Compulsory,
+  correct_func = function (self, from, to)
+    return from:getMark("@populace_attackrange")
+  end,
+}
+local populace_distance = fk.CreateDistanceSkill{
+  name = "#populace_distance",
+  frequency = Skill.Compulsory,
+  correct_func = function(self, from, to)
+    return to:getMark("@populace_distance") - from:getMark("@populace_distance")
+  end,
+}
+PopulaceSkill:addRelatedSkill(populace_attackrange)
+PopulaceSkill:addRelatedSkill(populace_distance)
+Fk:addSkill(PopulaceSkill)
+
+local WeaponPopulace = fk.CreateWeapon{
+  name = "&weapon1__populace",
+  suit = Card.Heart,
+  number = 1,
+  dynamic_equip_skills = function (self, player)
+    if player then
+      return {Fk.skills["#populace_skill"]}
+    end
+  end,
+}
+local ArmorPopulace = fk.CreateWeapon{
+  name = "&armor1__populace",
+  suit = Card.Diamond,
+  number = 1,
+  dynamic_equip_skills = function (self, player)
+    if player then
+      return {Fk.skills["#populace_skill"]}
+    end
+  end,
+}
+local DefensiveHorsePopulace = fk.CreateWeapon{
+  name = "&defensive_horse1__populace",
+  suit = Card.Club,
+  number = 1,
+  dynamic_equip_skills = function (self, player)
+    if player then
+      return {Fk.skills["#populace_skill"]}
+    end
+  end,
+}
+local OffensiveHorsePopulace = fk.CreateWeapon{
+  name = "&offensive_horse1__populace",
+  suit = Card.Spade,
+  number = 1,
+  dynamic_equip_skills = function (self, player)
+    if player then
+      return {Fk.skills["#populace_skill"]}
+    end
+  end,
+}
+extension:addCard(WeaponPopulace)
+extension:addCard(ArmorPopulace)
+extension:addCard(DefensiveHorsePopulace)
+extension:addCard(OffensiveHorsePopulace)
+for _, sub_type in ipairs({"weapon", "armor", "defensive_horse", "offensive_horse"}) do
+  local APopulace = fk.CreateArmor{
+    name = "&"..sub_type.."2__populace",
+    number = 1,
+    dynamic_equip_skills = function (self, player)
+      if player then
+        return {Fk.skills["#populace_skill"]}
+      end
+    end,
+  }
+  local DPopulace = fk.CreateDefensiveRide{
+    name = "&"..sub_type.."3__populace",
+    number = 1,
+    dynamic_equip_skills = function (self, player)
+      if player then
+        return {Fk.skills["#populace_skill"]}
+      end
+    end,
+  }
+  local OPopulace = fk.CreateOffensiveRide{
+    name = "&"..sub_type.."4__populace",
+    number = 1,
+    dynamic_equip_skills = function (self, player)
+      if player then
+        return {Fk.skills["#populace_skill"]}
+      end
+    end,
+  }
+  APopulace.package = extension
+  DPopulace.package = extension
+  OPopulace.package = extension
+  Fk:addCard(APopulace)
+  Fk:addCard(DPopulace)
+  Fk:addCard(OPopulace)
+end
+Fk:loadTranslationTable{
+  ["populace"] = "众",
+  ["#populace_skill"] = "众",
+  ["@populace_attackrange"] = "攻击范围+",
+  ["@populace_distance"] = "距离±",
+}
+for i = 1, 4, 1 do
+  Fk:loadTranslationTable{
+    ["weapon"..i.."__populace"] = "众",
+    [":weapon"..i.."__populace"] = "装备牌·武器/防具/坐骑<br/><b>装备技能</b>：锁定技，当你受到伤害后，你出牌阶段使用【杀】次数上限+1，"..
+    "攻击范围+1。",
+    ["armor"..i.."__populace"] = "众",
+    [":armor"..i.."__populace"] = "装备牌·武器/防具/坐骑<br/><b>装备技能</b>：锁定技，当你受到伤害后，你摸两张牌。",
+    ["defensive_horse"..i.."__populace"] = "众",
+    [":defensive_horse"..i.."__populace"] = "装备牌·武器/防具/坐骑<br/><b>装备技能</b>：锁定技，当你受到伤害后，你的手牌上限+1。",
+    ["offensive_horse"..i.."__populace"] = "众",
+    [":offensive_horse"..i.."__populace"] = "装备牌·武器/防具/坐骑<br/><b>装备技能</b>：锁定技，当你受到伤害后，其他角色计算与你距离+1，"..
+    "你计算与其他角色距离-1。",
+  }
+end
 
 return extension
