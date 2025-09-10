320f971cb6c163d6136dc3b1f53a48abde177af3
lysair <lysair@126.com>
2025-09-10 22:11:33 +0800

    init

--- pkg/sxfy/init.lua
+++ pkg/sxfy/init.lua
@@ -2,6 +2,7 @@ local extension = Package:new("sixiangfengyin")
 extension.extensionName = "offline"
 
 extension:loadSkillSkelsByPath("./packages/offline/pkg/sxfy/skills")
+extension.game_modes_whitelist = { 'nos_heg_mode', 'new_heg_mode' }
 
 Fk:loadTranslationTable{
   ["sixiangfengyin"] = "线下-四象封印",
